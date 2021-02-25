e6bf0f5238a20da23f083ec27292bbe3dea7b2d2
Break xD <55211911+BreakXD@users.noreply.github.com>
2021-02-24 23:32:39 -0300

    Translate the result to english from portuguese

--- api.php
+++ api.php
@@ -1,9 +1,14 @@
-<?php
-
-$code = substr(uniqid(rand()), 0, 5);
-
-echo $code;
-
-
-
-?>
+<?php 
+$user = $_GET['user'];
+$ch = curl_init('https://www.chess.com/callback/user/valid?username='.$user);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+$result = curl_exec($ch); 
+curl_close($ch); 
+$result = json_decode($result, 1); 
+if($result['valid'] !== false){
+	echo $user.' is available!';
+}else{
+	echo $user.' is in use!';
+}	
